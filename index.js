1964ec578150385dd947fb5854ae1dbf2f1a4c54
Thomas Wade <w1220w@126.com>
2017-07-11 17:35:26 +0800

    sync

--- index.js
+++ index.js
@@ -10,13 +10,14 @@ var routes = require('./routes');
 var pkg = require('./package');
 var winston = require('winston');
 var expressWinston = require('express-winston');
-
+var  cons = require('consolidate');
 var app = express();
 
 // 设置模板目录
 app.set('views', path.join(__dirname, 'views'));
 // 设置模板引擎为 ejs
 app.set('view engine', 'ejs');
+
 
 // 设置静态文件目录
 app.use(express.static(path.join(__dirname, 'public')));
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ module.exports = function (app) {
   app.get('/', function (req, res) {
     res.redirect('/posts');
   });
+
   app.use('/signup', require('./signup'));
   app.use('/signin', require('./signin'));
   app.use('/signout', require('./signout'));
